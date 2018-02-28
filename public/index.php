982e8bf91b1225487bd601d82aeee45b85e9410f
Daniel Hensby <dhensby@silverstripe.com>
2018-02-28 13:53:16 +0000

    Swap order of autoload searching

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ use SilverStripe\Core\CoreKernel;
 use SilverStripe\Core\Startup\ErrorControlChainMiddleware;
 
 // Find autoload.php
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require __DIR__ . '/vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
 } else {
     header('HTTP/1.1 500 Internal Server Error');
     echo "autoload.php not found";
